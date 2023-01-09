6a8e190fe74fdce9fa5178dd4fc9ea2d754fcd08
id7300 <id7300@yahoo.co.jp>
2023-01-09 12:06:33 +0900

    .gitignore added

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+    console.log("ユーザーが接続しました");
+
+    socket.on("chat message", (msg) => {
+        // consle.log("メッセージ:" + meg );
+        io.emit("chat message", msg);
+    });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log("listening on 3000");
+});
